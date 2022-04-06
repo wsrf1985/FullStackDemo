eff9cf306848f47df92096c8c4fddc0593bb6a7f
Peng Liu <wsrf1986@gmail.com>
2022-04-05 22:23:14 -0400

    Add project files.

---
+++ ITSMCodingTest/Controllers/HomeController.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using ITSMCodingTest.Helpers;
+using ITSMCodingTest.Models;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+
+namespace ITSMCodingTest.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private AddressBookEntities dbContext = new AddressBookEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Retrieves a list of countries from the countries.json helper.
+        /// Original API from https://restcountries.eu/rest/v2/all
+        /// Used for the Country selector
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetCountries()
+        {
+            try
+            {
+                // Read the countries.json file within the Helpers folder and map to a list of CountryView, sorted alphabetically
+                // << YOUR CODE HERE >>
+                List<CountryView> items = new List<CountryView>();
+                string json = System.IO.File.ReadAllText(HostingEnvironment.MapPath(@"~/Helpers/countries.json"));
+                items = JsonConvert.DeserializeObject<List<CountryView>>(json);
+                return SuccessResult(items);
+            }
+            catch (Exception e)
+            {
+                return FailedResult(e);
+            }
+        }
+
+        /// <summary>
+        /// Adds a new entry into the AddressRecords table using the AddressBookEntities database framework. Once the record is added, the generated Id of the record is returned back.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<JsonResult> AddEntry()
+        {
+            try
+            {
+                // Add a new AddressRecord entry to the Entities
+                // << YOUR CODE HERE >>
+                var recordId = await Task.Run(() => {
+                    AddressRecord record = dbContext.AddressRecords.Add(new AddressRecord
+                    {
+                        FirstName = "New",
+                        LastName = "New"
+                    });
+                    dbContext.SaveChanges();
+                    return record.Id;
+                });
+                return SuccessResult(recordId);
+            }
+            catch (Exception e)
+            {
+                return FailedResult(e);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all of the AddressRecord records, sorted alphabetically by last name and then by first name.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<JsonResult> GetAllEntries()
+        {
+            try
+            {
+                // Retrieve all of the records
+                // << YOUR CODE HERE >>
+                var records = await dbContext.AddressRecords.ToListAsync();
+                return SuccessResult(records);
+            }
+            catch (Exception e)
+            {
+                return FailedResult(e);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the record data provided in the object's Id property and then updates all properties in the record that are editable.
+        /// Remember, First and Last Name are required, and a photo may be provided. Check that the photo is a jpeg, gif, png, or bmp before allowing it to upload,
+        /// and ensure the image is less than 1MB to allow upload. If all checks pass, save the image as a PNG to the Uploads folder.
+        /// </summary>
+        /// <param name="recordData"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<JsonResult> SaveEntry(AddressRecord recordData)
+        {
+            try
+            {
+                using (var db = new AddressBookEntities())
+                {
+                    // Try to retrieve the record, and fail if the record doesn't exist
+                    // << YOUR CODE HERE >>
+                    AddressRecord record = await dbContext.AddressRecords.SingleOrDefaultAsync(x => x.Id == recordData.Id);
+                    if (record == null)
+                    {
+                        return FailedResult("Record doesn't exist in database");
+                    }
+                    // Validate that the required fields have values
+                    // << YOUR CODE HERE >>
+                    if (string.IsNullOrEmpty(recordData.FirstName) || string.IsNullOrEmpty(recordData.LastName))
+                    {
+                        return FailedResult("Please enter all required fields");
+                    }
+
+                    var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+                    for (var i = 0; i < Request.Files.Count; i++)
+                    {
+                        // Get the file if it exists, and if not, just carry on
+                        // << YOUR CODE HERE >>
+                        HttpPostedFileBase file = Request.Files[i];
+                        if (file == null)
+                        {
+                            continue;
+                        }
+                        // Check that the file has a valid name
+                        // << YOUR CODE HERE >>
+                        if (string.IsNullOrEmpty(file.FileName))
+                        {
+                            return FailedResult("File name is not valid");
+                        }
+                        // Validate the file size is less than 1MB
+                        // << YOUR CODE HERE >>
+                        if (file.ContentLength > 1024 * 1024)
+                        {
+                            return FailedResult("File size exceeds 1MB");
+                        }
+
+                        // Check that the photo is the correct type from the allowedExtensions
+                        // << YOUR CODE HERE >>
+                        string extension = Path.GetExtension(file.FileName);
+                        if (!allowedExtensions.Contains(extension))
+                        {
+                            return FailedResult("File type is not allowed");
+                        }
+
+                        // All is well, save the image to memory, and then drop it in the Uploads folder with the Id as the name in PNG format
+                        // << YOUR CODE HERE >>
+                        WriteFile(recordData.Id+".png",file.InputStream);
+                    }
+
+                    // Update all record properties and save changes
+                    // << YOUR CODE HERE >>
+                    await Task.Run(() => {
+                        record.LastName = recordData.LastName;
+                        record.FirstName = recordData.FirstName;
+                        record.City = recordData.City;
+                        record.Address = recordData.Address;
+                        record.AddressLine2 = recordData.AddressLine2;
+                        record.Country = recordData.Country == "null" ? string.Empty : recordData.Country;
+                        record.EmailAddress = recordData.EmailAddress;
+                        record.PhoneNumber = recordData.PhoneNumber;
+                        record.PostalZip = recordData.PostalZip;
+                        record.ProvinceState = recordData.ProvinceState;
+                        dbContext.SaveChanges();
+                    });
+                    return SuccessResult(recordData.Id);
+                }
+            }
+            catch (Exception e)
+            {
+                return FailedResult(e);
+            }
+        }
+
+        /// <summary>
+        /// Copies the contents of input to output. Doesn't close either stream.
+        /// </summary>
+        private void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+        }
+
+        private void WriteFile(string fileName, Stream inputStream)
+        {
+            string path = Server.MapPath("~/Uploads/");
+            if (System.IO.File.Exists(Path.Combine(path, fileName)))
+            {
+                System.IO.File.Delete(Path.Combine(path, fileName));
+            }
+            using (FileStream fs = new FileStream(Path.Combine(path, fileName), FileMode.CreateNew, FileAccess.Write))
+            {
+                CopyStream(inputStream, fs);
+            }
+            inputStream.Close();
+            inputStream.Flush();
+        }
+
+        /// <summary>
+        /// Deletes an entry from the Address Book
+        /// </summary>
+        /// <param name="recordId"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<JsonResult> DeleteEntry(int recordId)
+        {
+            try
+            {
+                // Get the record to delete, and fail if the record does not exist
+                // << YOUR CODE HERE >>
+                await Task.Run(() => {
+                    AddressRecord record = dbContext.AddressRecords.SingleOrDefaultAsync(x => x.Id == recordId).Result;
+                    dbContext.AddressRecords.Remove(record);
+                    dbContext.SaveChanges();
+                });
+                return SuccessResult();
+            }
+            catch (Exception e)
+            {
+                return FailedResult(e);
+            }
+        }
+    }
+}
---
+++ ITSMCodingTest/Scripts/app.js
@@ -0,0 +1,367 @@
+﻿// Initialize these variables which will be used globally throughout the application
+var addressBookRecords = [];
+var countryList = [];
+var currentRecord = null;
+
+
+// Generic Functions
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+function showLoading() {
+    $(".loading-overlay-panel").show();
+}
+
+function hideLoading() {
+    $(".loading-overlay-panel").hide();
+}
+
+function xhrErrorMessage(jqXhr) {
+    if (jqXhr.responseText !== undefined) {
+        var errMessage = $(jqXhr.responseText)[1].innerText;
+        if (errMessage !== null && errMessage !== undefined) {
+            return errMessage;
+        }
+    }
+    return "An Internal Server Error Occurred.";
+}
+
+function sortRecords() {
+    addressBookRecords.sort(function(a, b) {
+        var aLastChar = a.lastName.charAt(0);
+        var bLastChar = b.lastName.charAt(0);
+        if (aLastChar > bLastChar) {
+            return 1;
+        } else if (aLastChar < bLastChar) {
+            return -1;
+        } else {
+            var aFirstChar = a.firstName.charAt(0);
+            var bFirstChar = b.firstName.charAt(0);
+            if (aFirstChar > bFirstChar) {
+                return 1;
+            } else if (aFirstChar < bFirstChar) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    });
+}
+
+// Interactive Functions
+
+// Initializes the Address Book by loading the records and country dropdown.
+function initAddressBook() {
+    // Load all of the Entries and display them
+    // This ajax call is provided to you as a base for how your other calls need to be structured    
+    $.ajax({
+        url: "Home/GetAllEntries",
+        type: "GET",
+        dataType: "json"
+    }).done(function (data) {
+        if (data.result) {
+            addressBookRecords = data.resultSet;
+            sortRecords();
+            displayRecords();
+            return;
+        }
+        toastr.error(data.error, "Failed to get Load Records");
+    }).fail(function(xhr) {
+        toastr.error(xhrErrorMessage(xhr), "Failed to Load Records");
+    });
+
+    // Initialize the Country Selector by retriving via the GetCountries action and populating the dropdown and variable
+    // << YOUR CODE HERE >>
+    $.ajax({
+        url: "Home/GetCountries",
+        type: "GET",
+        dataType: "json"
+    }).done(function (data) {
+        if (data.result) {
+            countryList = data.resultSet;
+            $.each(countryList, function (index, value) {
+                // APPEND OR INSERT DATA TO SELECT ELEMENT.
+                $('#selectCountry').append('<option value="' + value.name + '">' + value.name + '</option>');
+            });
+            return;
+        }
+        toastr.error(data.error, "Failed to get Countries");
+    }).fail(function (xhr) {
+        toastr.error(xhrErrorMessage(xhr), "Failed to Countries");
+    });
+}
+
+// Displays the address book records, with an optional parameter to pre-select an ID of a record
+function displayRecords(preselectId) {
+    // << YOUR CODE HERE >>
+    $(".address-book-entries").empty();
+    $.each(addressBookRecords, function (index, value) {
+        if (index == 0 && !preselectId) {
+            preselectId = value.id;
+        }
+        $(".address-book-entries").append('<a id="left' + value.id + '" class="address-book-record" href="javascript:selectRecord(' + value.id +');"><div class="row  align-items-center"><div class= "col-3" ><img src="~/Content/Images/blankuser.png" onerror="this.src = \'Content/Images/blankuser.png\';" class="record-photo rounded-circle" id="recordPhoto"/></div ><div class= "col-9 text-left" ><h5>' + value.lastName+", " + value.firstName + '</h5></div ></div ></a>');
+    });
+    selectRecord(preselectId);
+}
+
+// Adds a new entry with the First and Last name of "New"
+function addNewEntry() {
+    // << YOUR CODE HERE >> 
+    showLoading();
+    $.ajax({
+        url: "Home/AddEntry",
+        type: "POST"
+    }).done(function (data) {
+        if (data.recordId) {
+            currentRecord = {};
+            currentRecord.id = data.recordId;
+            $("#inputFirstName").val("New");
+            $("#inputLastName").val("New");
+            hideLoading();
+            refreshAllRecords(data.recordId);
+            return;
+        }
+        toastr.error(data.error, "Failed to get Countries");
+        hideLoading();
+    }).fail(function (xhr) {
+        toastr.error(xhrErrorMessage(xhr), "Failed to Countries");
+        hideLoading();
+    });
+    
+}
+
+/**
+ * Function to refresh All Records with a selected record id Added by Peng
+ * @param {any} selectedId
+ */
+function refreshAllRecords(selectedId) {
+    $.ajax({
+        url: "Home/GetAllEntries",
+        type: "GET",
+        dataType: "json"
+    }).done(function (data) {
+        if (data.result) {
+            addressBookRecords = data.resultSet;
+            sortRecords();
+            displayRecords(selectedId);
+            return;
+        }
+        toastr.error(data.error, "Failed to get Load Records");
+    }).fail(function (xhr) {
+        toastr.error(xhrErrorMessage(xhr), "Failed to Load Records");
+    });
+}
+
+/**
+ * Get Current Record by Record Id
+ * @param {any} selectedId
+ */
+function getCurrentRecordById(recordId) {
+    if (recordId) {
+        currentRecord = addressBookRecords.find(function (item) { return item.id == recordId; });
+    } else {
+        currentRecord = null;
+    }
+}
+
+// Selects a record based on the element in the list or a record ID number
+function selectRecord(record) {
+    let recordId = 0;
+    // Check if the record is a number or if it's our <a> element that contains the record data, and get the ID accordingly
+    // << YOUR CODE HERE >>
+    if (typeof record == 'number') {
+        recordId = record;
+    }
+    // Hide all fields, as we're only going to show them if they have data
+    // << YOUR CODE HERE >>
+    $("#displayAddressColumn").hide();
+    $("#displayContactColumn").hide();
+    $("#displayAddress").hide();
+    $("#displayPhoneItem").hide();
+    $("#displayEmailItem").hide();
+    $("#displayPhone").hide();
+    $("#displayEmail").hide();
+    // Once we've gotten the record from the addressBookRecords, display the content on the front end and enable the Edit button
+    // << YOUR CODE HERE >>
+    if (addressBookRecords && addressBookRecords.length > 0 && recordId > 0) {
+        $(".address-book-record").removeClass("active");
+        $("#left" + recordId).addClass("active");
+        getCurrentRecordById(recordId);
+
+        $("#displayName").html(currentRecord.firstName + " " + currentRecord.lastName);
+
+        let addressHtml = currentRecord.address ? currentRecord.address + "<br/>" : "";
+        addressHtml += currentRecord.addressLine2 ? currentRecord.addressLine2 + "<br/>" : "";
+        addressHtml += currentRecord.city ? currentRecord.city : "";
+        addressHtml += currentRecord.provinceState ? ", " + currentRecord.provinceState + "<br/>" : "";
+        addressHtml += currentRecord.postalZip ? currentRecord.postalZip + "<br/>" : "";
+        addressHtml += currentRecord.country ? currentRecord.country : "";
+        $("#displayAddress").html(addressHtml);
+
+        $("#displayPhone").text(currentRecord.phoneNumber);
+        $("#displayEmail").text(currentRecord.emailAddress);
+
+        $("#editButton").prop('disabled', false);
+    }
+    // Show all columns/items that have data
+    // << YOUR CODE HERE >>
+    if ($("#displayAddress").html().length > 0) {
+        $("#displayAddressColumn").show();
+        $("#displayAddress").show();
+    }
+
+    if ($("#displayPhone").text().length > 0) {
+        $("#displayPhone").show();
+        $("#displayPhoneItem").show();
+        $("#displayContactColumn").show();
+    }
+
+    if ($("#displayEmail").text().length > 0) {
+        $("#displayEmail").show();
+        $("#displayEmailItem").show();
+        $("#displayContactColumn").show();
+    }
+    // Display the panel
+    // << YOUR CODE HERE >>
+    $("#readOnlyDisplay").show();
+    $("#editableDisplay").hide();
+}
+
+// Edits a record when pressing the 'Edit' button or triggered after creating a new entry
+function editRecord(recordId) {
+    // Set the currentRecord from the addressBookRecords
+    // << YOUR CODE HERE >>
+    getCurrentRecordById(recordId);
+
+    // Populate the editable fields
+    // << YOUR CODE HERE >>
+    if (currentRecord) {
+        $("#inputFirstName").val(currentRecord.firstName);
+        $("#inputLastName").val(currentRecord.lastName);
+        $("#inputAddress").val(currentRecord.address);
+        $("#inputAddressLine2").val(currentRecord.addressLine2);
+        $("#inputCity").val(currentRecord.city);
+        $("#inputProvinceState").val(currentRecord.provinceState);
+        $("#selectCountry").val(currentRecord.country);
+        $("#inputPhoneNumber").val(currentRecord.phoneNumber);
+        $("#inputEmailAddress").val(currentRecord.emailAddress);
+    // Show the panel and disable the Edit button
+    // << YOUR CODE HERE >>
+        $("#editableDisplay").show();
+        $("#readOnlyDisplay").hide();
+        $("#editButton").prop('disabled', true);
+    }    
+}
+
+// Saves the current editing entry
+function saveEntry() {
+    // Validate all entries which contain the "data-required" attribute
+    // If validation fails, display an error message and an indicator on the fields
+    // << YOUR CODE HERE >>
+    let validateFlag = true;
+    showLoading();
+    $.each($("#editableDisplay input"), function (index, value) {
+        let attr = $(this).attr('data-required');
+
+        // For some browsers, `attr` is undefined; for others,
+        // `attr` is false.  Check for both.
+        if (typeof attr !== 'undefined' && attr !== false) {
+            // ...            
+            if ($(this).val() == null || $(this).val() == "") {
+                toastr.error($("label[for*='" + $(this).attr("id") + "']").text().replace(":", "") + " is required");
+                $(this).css("border-color", "red");
+                validateFlag = false;
+            } else {
+                $(this).css("border-color", "#ced4da");
+            }
+        }
+    });
+
+    if (validateFlag) {
+    // Update the currentRecord with the data entered in
+    // << YOUR CODE HERE >>
+        currentRecord.firstName = $("#inputFirstName").val();
+        currentRecord.lastName = $("#inputLastName").val();
+        currentRecord.address = $("#inputAddress").val();
+        currentRecord.addressLine2 = $("#inputAddressLine2").val();
+        currentRecord.city = $("#inputCity").val();
+        currentRecord.provinceState = $("#inputProvinceState").val();
+        currentRecord.postalZip = $("#inputPostalZip").val();
+        currentRecord.country = $("#selectCountry").val();
+        currentRecord.phoneNumber = $("#inputPhoneNumber").val();
+        currentRecord.emailAddress = $("#inputEmailAddress").val();
+
+    // Create a $formData object, attach the photo if necessary, and include the currentRecord as part of the payload
+    // << YOUR CODE HERE >>
+        let formData  = new FormData();
+        $.each(jQuery('#photoUpload')[0].files, function (i, file) {
+            formData.append('file-' + i, file);
+        });
+        formData.set("id", currentRecord.id);
+        formData.set("firstName", currentRecord.firstName);
+        formData.set("lastName", currentRecord.lastName);
+        formData.set("address", currentRecord.address);
+        formData.set("addressLine2", currentRecord.addressLine2);
+        formData.set("city", currentRecord.city);
+        formData.set("provinceState", currentRecord.provinceState);
+        formData.set("postalZip", currentRecord.postalZip);
+        formData.set("country", currentRecord.country);
+        formData.set("phoneNumber", currentRecord.phoneNumber);
+        formData.set("emailAddress", currentRecord.emailAddress);
+
+    // Save the record, re-sort and display if successful
+    // << YOUR CODE HERE >>
+        $.ajax({
+            url: "Home/SaveEntry",
+            type: "POST",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+        }).done(function (data) {
+            if (data.result) {
+                refreshAllRecords(currentRecord.id);
+                hideLoading();
+                return;
+            }
+            toastr.error(data.error, "Failed to Save Record");
+            hideLoading();
+        }).fail(function (xhr) {
+            toastr.error(xhrErrorMessage(xhr), "Failed to Save Record");
+            hideLoading();
+        });
+    }
+    
+}
+
+// Deletes the current editing entry
+function deleteEntry() {
+    // Delete via the DeleteEntry action in the controller.
+    // On successful deletion, remove the record from the addressBookRecords array and update the display
+    // << YOUR CODE HERE >>
+    if (currentRecord) {
+        showLoading();
+        $.ajax({
+            url: "Home/DeleteEntry",
+            type: "POST",
+            dataType: "json",
+            data: {
+                recordId: currentRecord.id
+            }
+        }).done(function (data) {
+            if (data.result) {
+                hideLoading();
+                refreshAllRecords();
+                return;
+            }
+            toastr.error(data.error, "Failed to Delete Record");
+            hideLoading();
+        }).fail(function (xhr) {
+            toastr.error(xhrErrorMessage(xhr), "Failed to Delete Record");
+            hideLoading();
+        });
+    }
+    
+}
